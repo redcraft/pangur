5e2c999ac748f2466ed2c5a630f9e122de706470
Maxim Gurkin <redmax3d@gmail.com>
2015-03-10 04:31:09 +0500

    Added empty pages

--- app.js
+++ app.js
@@ -2,12 +2,29 @@
 
 $(document).ready(function(){
 
-    var setEvents = function(button, background) {
-        $(button)
-        .mouseover(function(){$(background).stop().fadeTo(400, 1)})
-        .mouseout(function(){$(background).stop().fadeTo(400, 0)});
+    var setEvents = function(id) {
+        var buttonId = "#" + id + "-button";
+        var backgroundId = "#" + id + "-background";
+        var pageId = "#" + id + "-page";
+        var active = false;
+        $(buttonId)
+            .mouseover(function(){if(!active)$(backgroundId).stop().fadeTo(400, 1)})
+            .mouseout(function(){if(!active)$(backgroundId).stop().fadeTo(400, 0)})
+            .click(function() {
+                    active = true;
+                    $(backgroundId).css("opacity", 1);
+                    $("header").addClass("header-rise");
+                    $(pageId).show('slide', {direction: 'down'}, 500);
+                    $(pageId).find(".pangur-thin-arrow-down").click(function() {
+                        active = false;
+                        $("header").removeClass("header-rise");
+                        $(pageId).hide('slide', {direction: 'down'});
+                        $(backgroundId).css("opacity", 0);
+                    });
+            }
+        );
     };
-    setEvents("#about", "#about-background");
-    setEvents("#portfolio", "#portfolio-background");
-    setEvents("#contact", "#contact-background");
+    setEvents("about");
+    setEvents("portfolio");
+    setEvents("contact");
 });
